d58422d0bf7075972c0336c2815ab1cb7fb62361
Bruno <brunosalvador145@gmail.com>
2023-10-28 09:54:52 -0300

    feat: create welcome page initial in frontend

---
+++ frontend/src/pages/Welcome/index.jsx
@@ -0,0 +1,20 @@
+import "./Welcome.css"
+import { ImNewspaper } from "react-icons/im"
+
+const Welcome = () => {
+    return (
+        <>
+        <div className="welcome mx-3">
+            <div className="mt-4">
+                <h1>Bem vindo a Papelaria HIPO</h1>
+                <p className="p-0 m-0 text-center">Abra o menu para acessar os dados do sistema</p>
+            </div>
+            <div className="container-icon">
+                <ImNewspaper />
+            </div><br/>
+        </div>
+        </>
+    )
+}
+
+export default Welcome;
