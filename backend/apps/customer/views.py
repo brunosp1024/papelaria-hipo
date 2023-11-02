bec33c2ce38a953ad3ee5b97d8a36ce6ddb52421
Bruno <brunosalvador145@gmail.com>
2023-11-01 21:46:53 -0300

    feat: create crud view to customer

--- backend/apps/customer/views.py
+++ backend/apps/customer/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Customer
+from .serializers import CustomerSerializer
+
+
+class CustomerViewSet(ModelViewSet):
+    """ Implements all actions to customers: list, create, retrieve, update e delete """
+
+    serializer_class = CustomerSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Customer.objects.all()
