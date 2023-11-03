dfdb8a28fa4eed10d39436dc895ee00e2a5267a3
Bruno <brunosalvador145@gmail.com>
2023-11-03 16:56:03 -0300

    refactor: update select dropdown component

--- frontend/src/pages/Sales/components/SelectDropdown.jsx
+++ frontend/src/pages/Sales/components/SelectDropdown.jsx
@@ -1,16 +1,7 @@
-import { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Select from 'react-select';
 
 const SelectDropdown = (props) => {
-    const [list, setList] = useState('');
-
-    useEffect(() => {
-        const items = props.items.map((p) => (
-            { value: p.id, label: `${p.id} - ${p.name}` }
-        ))
-        setList(items)
-    }, [props.url, props.items]);
 
     return (
         <div className="mb-3">
@@ -19,7 +10,7 @@ const SelectDropdown = (props) => {
             </Form.Label>
             <Select 
                 placeholder="Selecione..."
-                options={list}
+                options={props.items}
                 id={props.id}
                 value={props.value}
                 onChange={e => props.onChanged(e)}
