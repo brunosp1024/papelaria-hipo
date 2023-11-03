2ae7335ce070f5c812b7af0dd1b5dba38ceb3c89
Bruno <brunosalvador145@gmail.com>
2023-11-03 17:22:39 -0300

    feat: add Sale and SaleItem models in django admin

--- backend/apps/sale/admin.py
+++ backend/apps/sale/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.sale.models.sale_item import SaleItem
+from .models import Sale
+
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('invoice', 'seller', 'customer', 'date')
+    list_display_links = ('invoice',)
+    search_fields = ('invoice', 'seller', 'customer')
+    raw_id_fields = ('seller', 'customer')
+    list_per_page = 20
+
+admin.site.register(Sale, SaleAdmin)
+
+
+class SaleItemAdmin(admin.ModelAdmin):
+    list_display = ('sale', 'product', 'quantity')
+    list_display_links = ('sale',)
+    search_fields = ('sale__invoice', 'product__description')
+    raw_id_fields = ('sale', 'product')
+    list_per_page = 20
+
+admin.site.register(SaleItem, SaleItemAdmin)
+
