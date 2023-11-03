5c6fc3bd8ed746a7b98819d93f795d79d103529d
Bruno <brunosalvador145@gmail.com>
2023-11-03 17:02:38 -0300

    refactor: update layout of data table component

--- frontend/src/pages/Commissions/List/components/DataTable/DataTable.jsx
+++ frontend/src/pages/Commissions/List/components/DataTable/DataTable.jsx
@@ -1,6 +1,7 @@
 import "./DataTable.css"
 import Table from "react-bootstrap/esm/Table"
 import { formatToCurrencyBr } from "../../../../../Utils/formatCurrency"
+import { TbMoodEmpty } from "react-icons/tb";
 
 
 const DataTable = ({commissionsList, showDataTable}) => {
@@ -16,22 +17,26 @@ const DataTable = ({commissionsList, showDataTable}) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {commissionsList.map(commission => {
+                    {commissionsList.length ? commissionsList.map(commission => {
                         return (
                             <tr key={commission.id}>
                                 <td>{commission.sellerCode}</td>
-                                <td colSpan={2}>{commission.seller}</td>
+                                <td colSpan={2}>{commission.sellerName}</td>
                                 <td>{commission.salesTotal}</td>
                                 <td>{formatToCurrencyBr(commission.total)}</td>
                             </tr>
                         )
-                    })}
+                        }) : <tr><td className="py-4 text-center" colSpan={6}>
+                            Lista vazia <TbMoodEmpty className="fs-4"/></td></tr>
+                    }
+                    {commissionsList.length ?
+                        <tr>
+                            <th colSpan={4}>Total de commissões do período</th>
+                            <th >{formatToCurrencyBr(commissionsList.reduce((total, c) => {return total + parseFloat(c.total)}, 0))}</th>
+                        </tr> : null
+                    }
                 </tbody>
             </Table>
-            <div className="Info-commissions d-flex justify-content-between">
-                <th colspan='4'>Total de commissões do período</th>
-                <th >{formatToCurrencyBr(commissionsList.reduce((total, c) => {return total + c.total}, 0))}</th>
-            </div>
         </div>
     )
 }
