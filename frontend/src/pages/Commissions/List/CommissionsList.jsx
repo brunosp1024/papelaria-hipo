bbb827a5a6697cdce0e872273eca34a005aa2e05
Bruno <brunosalvador145@gmail.com>
2023-11-03 17:05:01 -0300

    refactor: update axios to get data commissions in api

--- frontend/src/pages/Commissions/List/CommissionsList.jsx
+++ frontend/src/pages/Commissions/List/CommissionsList.jsx
@@ -3,6 +3,8 @@ import { BiSearchAlt2 } from 'react-icons/bi';
 import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import DataTable from "./components/DataTable/DataTable";
+import axios from "../../../axios";
+import moment from "moment";
 
 const CommissionsList = () => {
     const [startDate, setStartDate] = useState();
@@ -10,91 +12,45 @@ const CommissionsList = () => {
     const [commissionsList, setCommissionsList] = useState([]);
     const [showDataTable, setShowDataTable] = useState(false);
 
-    const data = [
-        {
-            'sellerCode': 1,
-            'seller': 'Bruno Salvador',
-            'salesTotal': 15,
-            'total': 350.0
-        },
-        {
-            'sellerCode': 2,
-            'seller': 'Allan Rafael',
-            'salesTotal': 20,
-            'total': 450.0
-        },
-        {
-            'sellerCode': 3,
-            'seller': 'David Danilo',
-            'salesTotal': 25,
-            'total': 550.0
-        },
-        {
-            'sellerCode': 1,
-            'seller': 'Bruno Salvador',
-            'salesTotal': 15,
-            'total': 350.0
-        },
-        {
-            'sellerCode': 2,
-            'seller': 'Allan Rafael',
-            'salesTotal': 20,
-            'total': 450.0
-        },
-        {
-            'sellerCode': 3,
-            'seller': 'David Danilo',
-            'salesTotal': 25,
-            'total': 550.0
-        },
-        {
-            'sellerCode': 2,
-            'seller': 'Allan Rafael',
-            'salesTotal': 20,
-            'total': 450.0
-        },
-        {
-            'sellerCode': 3,
-            'seller': 'David Danilo',
-            'salesTotal': 25,
-            'total': 550.0
-        },
-        {
-            'sellerCode': 2,
-            'seller': 'Allan Rafael',
-            'salesTotal': 20,
-            'total': 450.0
-        },
-        {
-            'sellerCode': 3,
-            'seller': 'David Danilo',
-            'salesTotal': 25,
-            'total': 550.0
-        }
-    ]
-
     const getCommissions = () => {
-        // Implementar o axios
-        setCommissionsList(data)
+        axios.get('/commissions', {
+            params: {
+              start_date: moment(startDate).format('YYYY-MM-DD'),
+              end_date: moment(endDate).format('YYYY-MM-DD')
+            }
+        }).then(
+            response => {
+                const commissions = response.data.map((commission) => (
+                    {
+                        id: commission.seller_id,
+                        sellerCode: commission.seller_code,
+                        sellerName: commission.seller_name,
+                        salesTotal: commission.sales_count,
+                        total: commission.commission_total
+                    }
+                ))
+                setCommissionsList(commissions)
+            }
+        )
         setShowDataTable(true)
     }
 
     return (
         <div className='container card p-5 mt-5 shadow-sm'>
             <div className=''>
-                <h2 className='text-start' styl>Comissões</h2>
+                <h2 className='text-start'>Comissões</h2>
             </div>
             <hr />
             <div className="d-flex align-items-center justify-content-between mt-3">
                 <h3 className="color-primary">Relatório de comissões</h3>
                 <div className="d-flex">
-                    <input className="form-control me-3 cursor-pointer" type="date" onChange={date => setStartDate(date)}/>
-                    <input className="form-control me-3 cursor-pointer" type="date" onChange={date => setEndDate(date)}/>
+                    <input className="form-control me-3 cursor-pointer" type="date" onChange={date => setStartDate(date.target.value)}/>
+                    <input className="form-control me-3 cursor-pointer" type="date" onChange={date => setEndDate(date.target.value)}/>
                     <Button className={`btn-get-commissions ${startDate && endDate ? '' : 'disabled'}`} onClick={() => getCommissions()}><BiSearchAlt2 /></Button>
                 </div>
             </div>
             <div className={`commission-list ${showDataTable ? '' : ' d-flex align-items-center'}`}>
-                <h className={showDataTable ? 'd-none' : ''} >Para visualizar o relatório, selecione um período nos campos acima.</h>
+                <p className={showDataTable ? 'd-none' : ''} >Para visualizar o relatório, selecione um período nos campos acima.</p>
                 <DataTable showDataTable={showDataTable} commissionsList={commissionsList}/>
             </div>
         </div>
