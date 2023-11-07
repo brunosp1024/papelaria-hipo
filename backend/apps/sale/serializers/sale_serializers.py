0d03614db22c92007d4bd7ed0b6953290bfd9847
Bruno <brunosalvador145@gmail.com>
2023-11-07 18:19:54 -0300

    refactor: update code to save sale items

--- backend/apps/sale/serializers/sale_serializers.py
+++ backend/apps/sale/serializers/sale_serializers.py
@@ -3,6 +3,8 @@ from django.db import transaction
 
 from apps.sale.models import Sale, SaleItem
 from apps.sale.serializers import SaleItemSerializer
+from apps.sale.models import CommissionSetup
+from apps.product.models import Product
 
 
 class SaleSerializer(serializers.ModelSerializer):
@@ -29,19 +31,38 @@ class SaleSerializer(serializers.ModelSerializer):
         customer_id = validated_data.pop('customer')['customer_id']
 
         with transaction.atomic():
-            if sale_id:
-                sale = Sale.objects.filter(sale_id=sale_id)
-                sale.update(seller_id=seller_id, customer_id=customer_id, **validated_data)
-                sale = sale.first()
-                sale.sale_items.all().delete()
-            else:
-                sale = Sale.objects.create(
-                    seller_id=seller_id, customer_id=customer_id, **validated_data
-                )
-            
+            sale = self.create_or_update_sale(sale_id, seller_id, customer_id, validated_data)
+            commission_setup = CommissionSetup.objects.filter(active=True).first()
+            days = []
+            if commission_setup:
+                min_percentage = commission_setup.min_percentage
+                max_percentage = commission_setup.max_percentage
+                days = commission_setup.days
+
             for item in items_data:
                 product_id = item.pop('product')['product_id']
+                product = Product.objects.filter(product_id=product_id).first()
+                day_commission_percentage = product.commission_percentage
+
+                if str(sale.date.weekday()) in days:
+                    day_commission_percentage = \
+                        max(min_percentage, min(max_percentage, day_commission_percentage))
+                item['day_commission_percentage'] = day_commission_percentage
+
                 SaleItem.objects.create(sale=sale, product_id=product_id, **item)
             sale.commission_total = sum([item.commission_total for item in sale.sale_items.all()])
             sale.save()
+        return sale
+
+    def create_or_update_sale(self, sale_id, seller_id, customer_id, validated_data):
+        if sale_id:
+            sale = Sale.objects.filter(sale_id=sale_id)
+            sale.update(seller_id=seller_id, customer_id=customer_id, **validated_data)
+            sale = sale.first()
+            sale.sale_items.all().delete()
+        else:
+            sale = Sale.objects.create(
+                seller_id=seller_id, customer_id=customer_id, **validated_data
+            )
+
         return sale
